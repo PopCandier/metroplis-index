02278c1411bf4e8c1ac436497ee7d916c66dac38
Pop <824173124@qq.com>
2020-02-16 00:04:09 +0800

    授权中心添加

---
+++ metropolis-authorization/src/main/java/com/metropolis/authorization/bootstrap/AuthorizationApp.java
@@ -0,0 +1,21 @@
+package com.metropolis.authorization.bootstrap;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@ComponentScan(basePackages = "com.metropolis.authorization")
+@MapperScan(basePackages = "com.metropolis.authorization.dal.persistence")
+public class AuthorizationApp
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(AuthorizationApp.class);
+    }
+}
---
+++ metropolis-authorization/src/main/java/com/metropolis/authorization/realm/UserRealm.java
@@ -0,0 +1,36 @@
+package com.metropolis.authorization.realm;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+/**
+ * @author Pop
+ * @date 2020/2/15 22:17
+ */
+public class UserRealm extends AuthorizingRealm {
+
+    /**
+     * 获取身份权限信息
+     * @param principalCollection
+     * @return
+     */
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        return null;
+    }
+
+    /**
+     * 获取身份信息
+     * @param authenticationToken
+     * @return
+     * @throws AuthenticationException
+     */
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        return null;
+    }
+}
